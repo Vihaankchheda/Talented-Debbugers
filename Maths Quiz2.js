ce33ce643644d869153232f6fbd65e5071f452ab
Vihaankchheda <150892919+Vihaankchheda@users.noreply.github.com>
2023-11-15 13:01:14 +0530

    added Maths Quiz 1.html

---
+++ Maths Quiz2.js
@@ -0,0 +1,120 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Find the mode of the given data: 3,4,6,2,3,5,1,3,5,4,1,3,5,1,5,3,2,3,3,6 ",
+    answer: "3",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "x + 14 = 24. Find the value of <b> x </b>",
+    answer: "10",
+    options: [
+      "10",
+      "11",
+      "9",
+      "13"
+    ]
+  },
+    {
+    numb: 3,
+    question: "8y = 56. Find the value of <b> y </b>",
+    answer: "7",
+    options: [
+      "7",
+      "8",
+      "6",
+      "9"
+    ]
+  },
+    {
+    numb: 4,
+    question: "-22 = 7y + 6. Find the value of <b> y </b>",
+    answer: "-4",
+    options: [
+      "4",
+      "-4",
+      "-5",
+      "3"
+    ]
+  },
+    {
+    numb: 5,
+    question: "-8x = 72. Find the value of <b> x </b>",
+    answer: "-9",
+    options: [
+      "9",
+      "-9",
+      "6",
+      "10"
+    ]
+  },
+
+ {
+    numb: 6,
+    question: "A dice is thrown.Find the probability of getting an even number?",
+    answer: "1/2",
+    options: [
+      "1/2",
+      "2/3",
+      "4/5",
+      "5/6"
+    ]
+  },
+
+   {
+    numb: 7,
+    question: "Find the mode of the given data: 20,14,18,9,11,9,5,14,13.",
+    answer: "14 and 9",
+    options: [
+      "14 and 9",
+      "13 and 9",
+      "18 and 11",
+      "20 and 9"
+    ]
+  },
+
+   {
+    numb: 8,
+    question: "Find the mean of the given data: 50,46,49,55,50,51,56",
+    answer: "51",
+    options: [
+      "50",
+      "61",
+      "52",
+      "51"
+    ]
+  },
+
+   {
+    numb: 9,
+    question: "Find the range if the given data: 100,94,87,67,90,92,101,107 and 90.",
+    answer: "40",
+    options: [
+      "40",
+      "50",
+      "55",
+      "60"
+    ]
+  },
+
+   {
+    numb: 10,
+    question: "What is the probability of getting heads when coin is tossed? ",
+    answer: "1/2",
+    options: [
+      "3/5",
+      "1/4",
+      "1/3",
+      "1/2"
+    ]
+  },
+
+
+
+];
