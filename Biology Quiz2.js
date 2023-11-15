4f8cf9a0e81fd3ac7ef4dd215d8f9fed1d9f3a03
Vihaankchheda <150892919+Vihaankchheda@users.noreply.github.com>
2023-11-15 12:50:47 +0530

    added index.html

---
+++ Biology Quiz2.js
@@ -0,0 +1,120 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Through which of the following parts do <b> Fish </b> breathe?",
+    answer: "Gill",
+    options: [
+      "Spiracles",
+      "Gill",
+      "Lungs",
+      "Moist Skin"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following parts give support to the <b> Lungs </b>",
+    answer: "Diaphragm",
+    options: [
+      "Bronchioles",
+      "Bronchus",
+      "Diaphragm",
+      "None of the Above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the approximate percentage of oxygen in exhaled air?",
+    answer: "16%",
+    options: [
+      "17%",
+      "21%",
+      "16%",
+      "4%"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is the incorrect statement for <b> Breathing </b>?",
+    answer: "It is a Chemical Change",
+    options: [
+      "It occur outside the cells",
+      "There is no energy released",
+      "It is a Chemical Change",
+      "None of the Above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is the red-coloured <b> Protein </b> present in blood cells",
+    answer: "Haemoglobin",
+    options: [
+      "Haemoglobin",
+      "Chlorophyll",
+      "Vitamin",
+      "None of the Above"
+    ]
+  },
+
+ {
+    numb: 6,
+    question: "How many molecules of oxygen can combine with <b> Heamoglobin </b> ?",
+    answer: "4",
+    options: [
+      "12",
+      "2",
+      "8",
+      "4"
+    ]
+  },
+
+   {
+    numb: 7,
+    question: "Which of the following <b> Category </b> does frog comes under ?",
+    answer: "Amphibians",
+    options: [
+      "Amphibians",
+      "Reptile",
+      "Mammal",
+      "None of the above"
+    ]
+  },
+
+   {
+    numb: 8,
+    question: "__________ is the process by which undigested food is removed from the body in the form of faeces ?",
+    answer: "Egestion",
+    options: [
+      "Egestion",
+      "Digestion",
+      "Assimilation",
+      "Absorption"
+    ]
+  },
+
+   {
+    numb: 9,
+    question: "Through which of the following parts does <b> Amoeba </b> do the process of Digestion?",
+    answer: "Pseudopodia",
+    options: [
+      "Food Vocuole",
+      "Cyptoplasm",
+      "Pseudopodia",
+      "Nucleus"
+    ]
+  },
+
+   {
+    numb: 10,
+    question: "Which of the is the hardest part of the body? ",
+    answer: "Enamel",
+    options: [
+      "Ball-and-socket Bone",
+      "Collar Bone",
+      "Enamel",
+      "None of the Above"
+    ]
+  },
+
+
+
+];
